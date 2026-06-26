159a0f1d154686d164a9443d1947e1273ee96516
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2026-06-26 21:41:22 +0200

    Add tests for "rent movie" endpoint

--- src/main/java/com/tapeflicks/rentalstore/rental/RentalService.java
+++ src/main/java/com/tapeflicks/rentalstore/rental/RentalService.java
@@ -5,6 +5,7 @@ import com.tapeflicks.rentalstore.rental.dto.RentalResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 public class RentalService {
+  @Value("${rentalstore.rental.period-days:7}")
+  private int rentalPeriodDaysDefault;
 
   @Transactional
   public ResponseEntity<RentalResponse> rentMovie(String idempotencyKey, RentalRequest request) {
