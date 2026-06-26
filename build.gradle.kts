1e0ec19fb4bdae0676b5ffb9e3c60e840d10cdb0
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2026-06-26 21:14:02 +0200

    Add PMD plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.gradle.api.plugins.quality.Pmd
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.15"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.asciidoctor.jvm.convert") version "4.0.5"
     id("com.diffplug.spotless") version "8.6.0"
+    id("pmd")
 }
 
 group = "com.tapeflicks"
@@ -22,6 +25,11 @@ repositories {
 extra["snippetsDir"] = file("build/generated-snippets")
 extra["springCloudVersion"] = "2025.0.3"
 
+
+val swaggerVersion = "2.8.16"
+val apacheLang3Version18 = "3.18.0"
+val apacheCompressVersion = "1.24.0"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
@@ -29,12 +37,15 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
-	implementation("org.springframework.cloud:spring-cloud-bus")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$swaggerVersion")
+    constraints {
+        implementation("org.apache.commons:commons-lang3:$apacheLang3Version18") // force minimum version to avoid CVE
+        implementation("org.apache.commons:commons-compress:$apacheCompressVersion") // force minimum version to avoid CVE
+    }
+    implementation("org.springframework.cloud:spring-cloud-bus")
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
 	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
 	implementation("org.springframework.kafka:spring-kafka")
-	implementation("org.springframework.session:spring-session-jdbc")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
@@ -65,6 +76,12 @@ spotless {
         trimTrailingWhitespace()
         endWithNewline()
     }
+}
+
+configure<PmdExtension> {
+    isConsoleOutput = true
+    toolVersion = "7.0.0"
+    rulesMinimumPriority = 5
 }
 
 tasks.withType<Test> {
